2ffc671c3c867786ff3179f105027d5587e8d970
Russ Teicheira <19768623+rteicheira@users.noreply.github.com>
2025-05-11 12:25:06 -0700

    Update upgradeportainer.sh

--- portainer/upgradeportainer.sh
+++ portainer/upgradeportainer.sh
@@ -3,16 +3,25 @@
 ##########
 # Filename: upgradeportainer.sh
 # Location: $HOME/docker/portainer
-# Date updated: April 19, 2025
+# Date updated: May 08, 2025
 #
 # Upgrades the Portainer console and agent.
 ##########
 
-# Stop the current container
-sudo docker kill portainer
+# Pull the latest Portainer container
+{ echo -e "Pulling the latest Portainer container...";}
+docker pull portainer/portainer-ce:lts
 
-# Delete the current container
-sudo docker rm portainer
+# Stops the current Portainer container
+{ echo -e "\nStopping the Portainer container...";}
+docker kill portainer
 
-# Download upgrade and run Portainer
-sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:lts
+# Removes the current Portainer container
+{ echo -e "\nRemoving the Portainer container...";}
+docker rm portainer
+
+# Run Portainer
+{ echo -e "\nStarting Portainer...";}
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:lts
+
+{ echo -e "\nPortainer updated and running"; }
